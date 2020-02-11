3539ba583c4e3bff7fa1167d3707dce8ea1fa37e
CPTNPatenaude <59814891+CPTNPatenaude@users.noreply.github.com>
2020-02-10 23:55:43 -0500

    Add files via upload

---
+++ edm4466-devoir2.py
@@ -0,0 +1,40 @@
+# coding utf-8
+
+# Importation des modules complémentaires
+import json
+import csv
+import requests
+
+# Variables de base
+res = "devoir.csv"
+url = "http://jhroy.ca/uqam/lobby.json"
+
+# Log-in
+entete = {
+	"User-Agent":"Francois-Alexis Favreau - 819/620-7265",
+	"From":"cf991001@ens.uqam.ca"
+}
+
+req = requests.get(url,entete)
+print(req)
+
+if req.status_code != 200:
+	print("cmoi")
+
+# Moisson
+lobby = req.json()
+
+# Boucle
+n = 0
+
+for occ in lobby["registre"]:
+    if "lima" in str(occ):
+        print(occ)
+    n += 1
+
+# Créer CSV
+
+lobby = open(lobby,"a")
+relob = csv.writer(lobby)
+relob.writerow(lobby)
+# J'ai fais mon gros possible!
